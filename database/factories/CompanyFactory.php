b2945f926e38ba8820722c5bf91564ea27807253
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2020-01-14 19:46:10 +0100

    Create migration seede, factory of Company entyty

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+
+  $company_name = $faker-> company;
+  $company_web_site = str_replace(",","",$company_name);
+
+    return [
+        'name' =>  $company_name,
+        'email' => $faker-> companyEmail,
+        'logo' => $faker-> imageUrl,
+        'website' =>  'www.' . str_replace(" ","-",$company_web_site) .'.com'
+
+        // 'name' =>  $faker-> word,
+        // 'email' => $faker-> word,
+        // 'logo' => $faker-> word,
+        // 'website' =>  $faker-> word
+    ];
+});
